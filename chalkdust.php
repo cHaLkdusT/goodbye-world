3bfb34935d307d599c671eb0576eb3edc37546eb
cHaLkdusT <cHaLkdusT@users.noreply.github.com>
2020-08-12 16:33:10 +0800

    Added tribute file

---
+++ chalkdust.php
@@ -0,0 +1,13 @@
+<?php
+echo "I am honored and blessed to have known you sir! I'll never forget noong
+  tinanong mo ako kung ano plan ko para sa thesis namin. I said we'll be using
+  .NET then you smiled at me and replied  \"Eh alam mo na ang Java at .NET, 
+  bakit hindi mo kaya subukan ang PHP!\". That day, I accepted your challenge.";
+echo "and today...";
+echo "I'm not a Java developer.";
+echo "I'm not a .NET developer.";
+echo "I'm not a PHP developer.";
+echo "I'm now a mobile developer. (because I already know the other stuffs)";
+echo "Thank you for always pushing us to our limits. Because of you, I learned
+  to appreciate the IT world. You will forever be missed. Rest in peace sir!";
+?>
