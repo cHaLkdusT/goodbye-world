5d903f8695102306fa346a7f57ff3de3a3a964bb
Paul Catalan <24451518+polcats@users.noreply.github.com>
2020-08-13 19:39:03 +0800

    To my father figure, mentor, and inspiration.

--- theMainMan.js
+++ theMainMan.js
@@ -8,3 +8,4 @@ console.log(
 console.log('Being your student was an overall mental circus.');
 console.log('It was fun and challenging, and I am very happy to be one of your students.');
 console.log('I learned a lot, and I am proud to say that its all because of you.');
+console.log('You will be forever missed.');
