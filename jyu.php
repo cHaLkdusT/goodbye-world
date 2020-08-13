ecd59904d38ade7152cbaeca376308b3a83c0b9b
Judith Angeli <judith.angeli.yu@gmail.com>
2020-08-13 23:33:36 +0800

    Create jyu.php

---
+++ jyu.php
@@ -0,0 +1,28 @@
+<?php
+echo "
+    Alam niyo sir, yung subject nyo yung pinakamababang grade na nakuha ko sa buong time na nag-aral ako mula kinder 
+    hanggang college. Siya din mismo yung humila sa average ko kaya muntik na akong hindi masama sa list of honors. OOP po
+    yung subject na yun, elective pa. Iniisip ko, bakit hindi ko talaga sya ma-grasp noon? Yun pala, hindi ko kasi 
+    prinapractice ang programming sa bahay nun, kaya talagang hindi ko magegets! Kaka-FarmVille at Mafia Wars at Pet 
+    Society kasi e! Ang bait nyo pa nga sa mga students nyo, nagpapabonus question pa kayo! Hindi nga lang bonus yung 
+    answer, hindi lang din namin alam ang sagot hehe.
+    
+    Dahil din sa subject nyo na yun, napadoble kayod ako sa thesis. Habang panel defense namin, nagcocode ako sa harap
+    nyo kasi may bug ata nun. Imagine yung pressure na hindi pa ako ganun kagaling sa programming dati, tapos nanonood pa
+    yung teacher mo na feeling mo jinujudge ka na (haha). Ang taas ng anxiety level! Pero dahil dun sa pressure na yun
+    at para makapagmarcha sa harap na hindi ako ang huling tatawagin (kasi letter Y yung surname ko kaya kelangan ko humabol
+    sa honors para mejo nasa harap din kahit papano), napilitan ako magcode. Kaya naman pala namin!
+    
+    Naalala ko din, after pa nung 1st defense, gabi na yun and sabay sabay tayo lumabas ng ICTR office. Dun sa Gate3 tayo 
+    palabas nun, yakap ko yung laptop ko, tapos nadapa ako! Iniwas ko talaga yung laptop ko kasi hindi pa kami nag veversion 
+    control nun. Nasa laptop ko lahat lahat yung cinode ko sa harap nyo (nasa final final thesis folder). Hindi pwede 
+    masira laptop ko kasi hindi ko na din alam ulitin nun nung time na yun. So ayun, full force sa tuhod, pero atleast 
+    safe yung laptop ko. Tapos naalala ko ang tanong nyo -- 'Okay ka lang? Ilang palaka nahuli mo?' HAHAHA. Natawa nalang 
+    ako pero ang sakiiit ng tuhod ko (at ng dignity ko, nakakahiya!). 
+    
+    Pero dibale sir, hindi naman nasayang yung mga tinuro nyo. Nung nagwowork na ako, dun na nag-make sense lahat lahat
+     -- 'ah, eto lang pala yun?', 'ah naalala ko to, eto yung naturo sa OOP dati.'. Hindi man sya nagreflect into grades, 
+     nagreflect naman sya sa career ko. Thank you, sir. :-)
+     
+     Ngapala sir, may bug po yung 2020. Baka po pwede nyo na din i-fix on the side. 
+";
